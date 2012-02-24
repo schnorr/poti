a57b7e71b913e8a6dd29a9b52c654b87d5f311e7
Lucas Schnorr <schnorr@gmail.com>
2012-02-24 17:36:34 +0100

    this library is released under the LGPL version 3

--- src/poti.c
+++ src/poti.c
@@ -1,17 +1,17 @@
 /*
     This file is part of Poti
 
-    Poti is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Public License as published by
+    Poti is free software: you can redistribute it and/or modify it
+    under the terms of the GNU Lesser Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
     Poti is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Public License for more details.
+    GNU Lesser Public License for more details.
 
-    You should have received a copy of the GNU Public License
+    You should have received a copy of the GNU Lesser Public License
     along with Poti. If not, see <http://www.gnu.org/licenses/>.
 */
 #include "poti_private.h"
