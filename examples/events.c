c3d41ffb0c3c1e9fb7ff35fe13341cb6ed38a9f8
Lucas Mello Schnorr <schnorr@inf.ufrgs.br>
2015-11-12 20:34:59 -0200

    add GPL notice

--- examples/events.c
+++ examples/events.c
@@ -1,3 +1,19 @@
+/*
+    This file is part of Poti
+
+    Poti is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Poti is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Public License for more details.
+
+    You should have received a copy of the GNU Public License
+    along with Poti. If not, see <http://www.gnu.org/licenses/>.
+*/
 #include <poti.h>
 
 int main (int argc, char **argv)
